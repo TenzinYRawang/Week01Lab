2b943f4da121791e86052490315343327ac1f24f
729557 <729557@ELF6IT215881.ACDM.DS.SAIT.CA>
2017-09-14 13:07:12 -0600

    New Project Creation Created a new Java Application for HelloWorld3.

---
+++ src/hellowworld3/HellowWorld3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellowworld3;
+
+/**
+ *
+ * @author 729557
+ */
+public class HellowWorld3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
